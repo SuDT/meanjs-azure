5cb42cbdde0b6b1b79a2d0152e3e3f732430cd45
sud <sud@centos.localdomain>
2017-05-20 23:58:52 +0100

    added article comment

--- deploy.sh
+++ deploy.sh
@@ -112,7 +112,6 @@ selectNodeVersion
 # 3. Install NPM packages
 if [ -e "$DEPLOYMENT_TARGET/package.json" ]; then
   cd "$DEPLOYMENT_TARGET"
-  eval $NPM_CMD install eslint-plugin-react@^4.0.0 && 
   eval $NPM_CMD install --production
   eval $NPM_CMD install --only=dev
   exitWithMessageOnError "npm failed"
